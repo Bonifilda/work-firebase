5caa306bb0065dc22f007254afa0204fcfc70a17
Bonifilda <bonimani2020@gmail.com>
2025-11-11 18:56:04 +0200

    added readme to explain what Idid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,27 @@
- import type { Metadata } from "next";
+//  import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "TaskFlow - Firebase CRUD App",
+//   description: "Secure task manager with Firebase Auth & Firestore",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   );
+// }
+
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -17,6 +40,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-    </html>
-  );
+    </html>
+  );
 }
